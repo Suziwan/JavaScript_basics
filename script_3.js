b8d155b6ba7d6022abf79dfbe438e4d4e99dee9c
Suziwan <christe.suzanne@gmail.com>
2023-03-02 23:00:09 +0000

    Update pyramid

--- script_3.js
+++ script_3.js
@@ -1,5 +1,12 @@
-let floors;
+let floors = prompt('Welcome to the super pyramid! How many floors do you want?');
+let space = " ";
+let symbol = "#";
 
-function pyramid() {
-  floors = prompt('Welcome to the super pyramid! How many floors do you want?');
+if (floors > 25){
+  console.log(`Sorry, the pyramid cannot be higher than 25 floors`); 
+  }
+else {
+  for (var i = 0; i <= floors; i++){
+    console.log(space.repeat(floors-i) + symbol.repeat(i));
+  }
 }
