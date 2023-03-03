79eed72e5133bcd0ba67cda6b481ff587231362f
Suziwan <christe.suzanne@gmail.com>
2023-03-03 00:52:48 +0000

    Books

--- script_5.js
+++ script_5.js
@@ -1,4 +1,4 @@
-const books = [
+let books = [
   { title: 'Gatsby le magnifique', id: 133712, rented: 39 },
   { title: 'A la recherche du temps,perdu', id: 237634, rented: 28 },
   { title: 'Orgueil & Préjugés', id: 873495, rented: 67 },
@@ -12,3 +12,54 @@ const books = [
   { title: 'Voyage au centre de la Terre', id: 4656388, rented: 38 },
   { title: 'Guerre et Paix', id: 748147, rented: 19 }
 ];
+
+// Question 1
+console.log("------- Has every book been borrowed ?")
+
+let answer = true;
+for (var i = 0; i < books.length; i++) {
+  if (books[i].rented == 0) {
+    answer = false;
+    break;
+  }
+}
+
+console.log(answer)
+
+// Question 2
+console.log("------- Which book is the most borrowed ?")
+
+books.sort((a, b) => a.rented - b.rented);
+
+console.log(books[books.length-1])
+
+// Question 3
+console.log("------- Which one is the less borrowed ?")
+
+console.log(books[0])
+
+// Question 4
+console.log("------- Find book with ID: 873495")
+
+function first_idCheck(book) {
+  return book.id == 873495;
+}
+
+console.log(books.find(first_idCheck))
+
+// Question 5
+console.log("------- Delete book with ID: 133712")
+
+function second_idCheck(book) {
+  return book.id == 133712;
+}
+
+console.log(books.find(second_idCheck))
+let index = books.findIndex(second_idCheck);
+delete books[index];
+console.log(books);
+
+// Question 6
+console.log("------- Sort books by alphabetic order (without ID 133712)")
+
+console.log(books.sort((a, b) => a.title.localeCompare(b.title)))
